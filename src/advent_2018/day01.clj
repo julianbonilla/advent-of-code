d655c1336a3e40633b229636c5f82ad47bb2a018
Julian Bonilla <julian.bonilla@rackspace.com>
2019-01-04 10:50:44 -0500

    Alternative to thread first/last macro

--- src/advent_2018/day01.clj
+++ src/advent_2018/day01.clj
@@ -1,7 +1,7 @@
 (ns advent-2018.day01
   (:require
-   [clojure.string :as str]
-   [clojure.java.io :as io]))
+   [clojure.java.io :as io]
+   [clojure.string :as str]))
 
 (def input (slurp (io/resource "advent_2018/day01/input")))
 
@@ -10,7 +10,12 @@
       (str/split #"\n")
       (->> (map #(Integer/parseInt %)))))
 
+(def numz
+  (map #(Integer/parseInt %) (str/split input #"\n")))
+
 (defn part-1 []
   (reduce + 0 nums))
 
 #_(part-1)
+
+(defn part-2 [])
