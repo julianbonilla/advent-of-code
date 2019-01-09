091d520ba7a4c5a1a16f85feeb0873a921d1cbaf
Julian Bonilla <julian.bonilla@rackspace.com>
2019-01-09 15:37:41 -0500

    Day 1, part 2

--- src/advent_2018/day01.clj
+++ src/advent_2018/day01.clj
@@ -18,4 +18,26 @@
 
 #_(part-1)
 
-(defn part-2 [])
+;; +1, -2, +3, +1
+;; keep frequency count
+;; if frequency count seen twice return the frequency
+;; else recur
+
+(def data [+1, -2, +3, +1])
+
+(defn part-two [data]
+  (loop [freqs #{}
+         acc 0
+         coll (cycle data)
+         next-sum (+ acc (first coll))]
+    (if (contains? freqs next-sum)
+      next-sum
+      (recur (conj freqs next-sum)
+             next-sum
+             (rest coll)
+             (+ next-sum (first (rest coll)))))))
+
+(part-two data)
+(part-two nums)
+
+
