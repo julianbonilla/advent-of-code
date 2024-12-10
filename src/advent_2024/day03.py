28240f5398cb8778bad5ed58633b46e1f779b1da
Julian Bonilla <julianbonilla@users.noreply.github.com>
2024-12-10 12:59:31 -0500

    Day 3, 2024

--- src/advent_2024/day03.py
+++ src/advent_2024/day03.py
@@ -20,9 +20,28 @@ with open('day03-input.txt', 'r') as file:
 regex = 'mul\\((\\d{1,3}),(\\d{1,3})\\)'
 valid_instructions = re.findall(regex, memory)
 
-# print(valid_instructions)
-
 # sum all the mul instructions
 sum = sum([int(x) * int(y) for x, y in valid_instructions])
 
 print('Part 1:', sum)
+
+# Part 2 - handle 'do()' and 'don't()' instructions which enable/disable mul instructions
+regex_do_dont = 'mul\\((\\d{1,3}),(\\d{1,3})\\)|(do\\(\\))||(don\'t\\(\\))'
+instructions = re.findall(regex_do_dont, memory)
+
+do = True
+valid_instructions_part_2 = []
+for instruct in instructions:
+    if instruct[2]:
+        do = True
+    if instruct[3]:
+        do = False
+    if do and instruct[0]:
+        valid_instructions_part_2.append(instruct[:2])
+
+# sum all the mul instructions
+sum = 0
+for x, y in valid_instructions_part_2:
+    sum += int(x) * int(y)
+
+print('Part 2:', sum)
